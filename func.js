1a678343f69f7610123a53d2f1ae75e0a5b6e28a
Zuzek16 <zuzekzych@gmail.com>
2024-12-07 20:32:33 +0100

    gradient side-bars; question interacivity

--- func.js
+++ func.js
@@ -1,16 +1,53 @@
+let ran = false;
+
 function validateAnwser() {
+
+  const radioBtn = document.getElementsByName("quiz");
+  let anwser = document.querySelector("input[type=radio]:checked").value;
+  
+  switch (anwser) {
+    case 0:
+    wrongAnwser();
+      break;
+    case 1:
+    wrongAnwser();
+      break;
+    case 3:
+    rightAnwser();
+      break;
+    case 4:
+    wrongAnwser();
+      break;
+    
+  }
+
+}
+
+function wrongAnwser(params) {
+  
+}
+
+function rightAnwser(params) {
   
 }
 
 function dontKnow () {
-  const fishPic = document.getElementById("fishImg");
-  delBtn();
-  fishPic.style.display = "flex";
-  fishPic.style.visibility = "visible";
-  alert("FISH! \n Look at their silly little eyes, and think of it. \n They just breathe air just like you, but they are submerged in water all their life. \n Crazy... \n and how do they see outside water?");
+  if (!ran) {
+    ran = true;
+    const fishPic = document.getElementById("fishImg");
+    delBtn();
+    fishPic.style.display = "flex";
+    fishPic.style.visibility = "visible";
+    alert("FISH! \n Look at their silly little eyes, and think of it. \n They just breathe air just like you, but they are submerged in water all their life. \n Crazy... \n and how do they see outside water?");
+    document.getElementById("story").append(document.createElement("p").innerText = "It doesn't get any betta than this");
+  } else {
+    return;
+  }
+ 
 }
 
 function doKnow () {
+  ran = true;
   console.log('ran do know');
   delBtn();
   const div = document.getElementById("doKnow");
